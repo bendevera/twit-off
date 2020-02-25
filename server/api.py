f5f553f381bcfc58741e1b05d40d7bab24163e08
bendevera <ben10devera@gmail.com>
2020-02-24 17:20:28 -0700

    Update API

--- server/api.py
+++ server/api.py
@@ -1,6 +1,6 @@
 from server import app as API, db  
 from server.models import User, Tweet 
-from flask import jsonify, make_response 
+from flask import jsonify, make_response, request
 import tweepy
 import os 
 import dotenv
@@ -14,25 +14,76 @@ twitter = tweepy.API(auth)
 
 @API.route('/api')
 def api_ping():
-    return jsonify({'status': 'success', 'message': 'Welcome to the twitoff API!'})
+    responseObject = {
+        'status': 'success', 
+        'message': 'Welcome to the twitoff API!'
+    }
+    return make_response(jsonify(responseObject)), 200
 
-@API.route('/api/user/<screen_name>')
-def api_user(screen_name):
-    # user = twitter.get_user(screen_name)
-    # tweets = twitter.user_timeline(user['id'])
-    tweets = twitter.user_timeline(screen_name)
-    data = []
-    for item in tweets:
-        item = {
-            "author_name": item.author.screen_name,
-            "author_img": item.author.profile_image_url,
-            "author_follow_count": item.author.followers_count,
-            "is_initial": item.in_reply_to_status_id,
-            "is_quote": item.is_quote_status,
-            "text": item.text,
-            "favorites": item.favorite_count,
-            "retweets": item.retweet_count,
-            "created": item.created_at
+
+@API.route('/api/users', methods=['POST', 'GET'])
+def users():
+    if request.method == 'GET':
+        users = User.query.all()
+        print(users)
+        responseObject = {
+            'status': 'success', 
+            'data': [user.to_json() for user in users]
         }
-        data.append(item)
-    return jsonify({'status': 'success', 'data': data})
+        return make_response(jsonify(responseObject)), 200
+    elif request.method == 'POST':
+        screen_name = request.get_json()['screen_name']
+        # user = twitter.get_user(screen_name)
+        # tweets = twitter.user_timeline(user['id'])
+        test_user = User.query.filter_by(screen_name=screen_name).first()
+        tweets = twitter.user_timeline(screen_name)
+        if len(tweets) > 0:
+            if test_user:
+                user = test_user
+                user.img = tweets[0].author.profile_image_url
+                user.followers = tweets[0].author.followers_count
+                user.description = tweets[0].author.description
+            else:
+                user = User(
+                    screen_name=screen_name, 
+                    img=tweets[0].author.profile_image_url, 
+                    followers=tweets[0].author.followers_count,
+                    description=tweets[0].author.description
+                )
+                db.session.add(user)
+            data = []
+            print(f"{user.screen_name} has {len(tweets)} tweets.")
+            for item in tweets:
+                item = Tweet(
+                    text=item.text,
+                    user_id=user.id
+                )
+                user.tweets.append(item)
+            db.session.commit()
+            responseObject = {
+                'status': 'success', 
+                'data': user.to_json()
+            }
+            return make_response(jsonify(responseObject)), 200
+        else:
+            responseObject = {
+                'status': 'failure', 
+                'message': 'no tweets from that screen_name'
+            }
+            return make_response(jsonify(responseObject)), 404
+        
+
+@API.route('/api/users/<id>')
+def users_by_id(id):
+    user = User.query.filter_by(id=id).first()
+    if user:
+        responseObject = {
+            'status': 'success',
+            'data': user.to_json_w_tweets()
+        }
+        return make_response(jsonify(responseObject)), 200
+    responseObject = {
+        'status': 'failure',
+        'message': 'No user with id {}'.format(id)
+    }
+    return make_response(jsonify(responseObject)), 404
--- server/models.py
+++ server/models.py
@@ -4,8 +4,30 @@ from server import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String)
+    screen_name = db.Column(db.String)
+    description = db.Column(db.String)
+    img = db.Column(db.String) 
+    followers = db.Column(db.Integer)
     tweets = db.relationship("Tweet")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.screen_name,
+            "description": self.description,
+            "img": self.img,
+            "followers": self.followers
+        }
+    
+    def to_json_w_tweets(self):
+        return {
+            "id": self.id,
+            "name": self.screen_name,
+            "description": self.description,
+            "img": self.img,
+            "followers": self.followers,
+            "tweets": [tweet.to_json() for tweet in self.tweets]
+        }
 
 
 class Tweet(db.Model):
@@ -13,3 +35,9 @@ class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     text = db.Column(db.String)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "text": self.text
+        }
